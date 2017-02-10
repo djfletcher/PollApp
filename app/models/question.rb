6fc2a8f812b9d435534fc8a71ee2a33cbf889d69
Daniel <daniel.j.fletcher2@gmail.com>
2017-02-09 16:41:59 -0800

    complete project

--- app/models/question.rb
+++ app/models/question.rb
@@ -27,4 +27,37 @@ class Question < ActiveRecord::Base
     through: :answer_choices,
     source: :responses
 
+  def results
+    votes = self.answer_choices.includes(:responses)
+    results = {}
+
+    votes.each do |answer_choice|
+      results[answer_choice.text] = answer_choice.responses.length
+    end
+
+    results
+  end
+
+  def results_sql
+    self.answer_choices.select("answer_choices.text, COUNT(responses.id) AS num_votes")
+      .joins("LEFT JOIN responses ON responses.answer_choice_id = answer_choices.id")
+      .where("answer_choices.question_id = ?", self.id)
+      .group("answer_choices.id")
+
+    # Below is a working SQL query
+
+    # PollApp_development.instance.execute(<<-SQL, question_id)
+    #   SELECT
+    #     answer_choices.*, COUNT(responses.answer_choice_id) AS num_votes
+    #   FROM
+    #     answer_choices
+    #   LEFT JOIN
+    #     responses ON responses.answer_choice_id = answer_choices.id
+    #   WHERE
+    #     answer_choices.question_id = ?
+    #   GROUP BY
+    #     answer_choices.id
+    # SQL
+  end
+
 end
